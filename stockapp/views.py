0b7d78bbf25202b0798fded9b7a4f51969772aef
Jake Obron <jbobron@gmail.com>
2015-04-02 19:22:19 -0700

    restructured app file structure, now using __init__.py, views.py for routing, and runserver.py to well..run the server

--- stockapp/views.py
+++ stockapp/views.py
@@ -1,3 +1,4 @@
+from stockapp import app
 from flask import Flask
 from flask import render_template
 from pymongo import Connection
@@ -5,17 +6,13 @@ import json
 from bson import json_util
 from bson.json_util import dumps
 
-
-app = Flask(__name__)
-
-
 MONGODB_HOST = 'localhost'
 MONGODB_PORT = 27017
 DBS_NAME = 'test_database'
 COLLECTION_NAME = 'apple' #CHANGE ME BASED ON WHAT DATA YOU WANT
 FIELDS = {"Date":True, "Open":True, "High":True, "Low":True, "Close":True, "Volume":True}
 
-# ROUTES IN PYTHON EXAMPLE: http://runnable.com/Uh4qRmSwz8cHAAAN/how-to-perform-advanced-routing-in-flask-for-python-and-routes
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -38,14 +35,15 @@ def trade():
 
 # @app.route("/ticker") #parse out additional endpoint to get tickr (ex:ticker/csco)
 # def trade():
-#     #parse request info from client
-#     #make request to quandle
-#     #wait for response 
-#     #send info to client
+    #parse request info from client
+    #make request to quandle
+    #wait for response 
+    #send info to client
     
-# @app.route("/user") #parse out additional endpoint to get user (ex:user/andy)
-# def returnUserInfo():
-#     #hit mongoDB
+@app.route("/users/<int:userid>/") #parse out additional endpoint to get user (ex:user/andy)
+def capture_value_int(userid):
+    return 
+    #hit mongoDB
 
 
 @app.route("/googleStockData/projects")
@@ -59,7 +57,3 @@ def googleStockData_projects():
     json_projects = json.dumps(json_projects, default=json_util.default)
     connection.disconnect()
     return json_projects
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000,debug=True)
